6395599ec7ec553940658e73a03ee9ed3682cfeb
mismelpoulout <69745379+mismelpoulout@users.noreply.github.com>
2021-11-21 00:56:37 -0300

    Update App.js

--- src/App.js
+++ src/App.js
@@ -23,6 +23,28 @@ class App extends Component {
   render() {
   return  (
     <div className="App container">
+    <nav className="navbar navbar-expand-lg navbar-light bg-dark">
+        <div className="container-fluid">
+          <a className="navbar-brand" href="#"><span>Literatura medica</span></a>
+          <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+            <span className="navbar-toggler-icon"></span>
+          </button>
+          <div className="collapse navbar-collapse" id="navbarNav">
+            <ul className="navbar-nav">
+              <li className="nav-item">
+                <a className="nav-link active" aria-current="page" href="#">Home</a>
+              </li>
+              <li className="nav-item">
+                <a className="nav-link" href="#">Features</a>
+              </li>
+              <li className="nav-item">
+                <a className="nav-link" href="#">Pricing</a>
+              </li>
+              
+            </ul>
+          </div>
+        </div>
+      </nav>
       <div className="jumbotron">
         <h1 className="lead text-center">Literatura medica</h1>
         <Buscador 
